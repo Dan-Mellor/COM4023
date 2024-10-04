3fe93165baf6da60b5398f131ddecaefec4f37d0
dmellor <dmellor@172.18.38.117>
2024-10-04 10:16:23 +0100

    help

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package javaapplication1;
+import java.util.Scanner;
+
 
 /**
  *
@@ -13,8 +15,25 @@ public class JavaApplication1 {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
+        //Exercise One
         System.out.println("Hello World");
     }
     
+        //Exercise Two 
+        Scanner reader = new Scanner(System.in);
+        System.out.println("What is your naem")
+        String name=reader.nextLine();
+        System.out.println("Your name was "+ name);
+        
+    
+   /** public static void main(String[] args) {
+        String name;
+        System.out.println("Hello, what's your name?");
+        name = InputReader.getString();
+        System.out.println("Hello " + name);
+    }
+    */
 }
