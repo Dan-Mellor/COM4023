a19d8d43a353b81026edb3c0cbbe383f7d340c3f
dmellor <dmellor@172.18.38.117>
2024-10-04 10:55:24 +0100

    help

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -26,8 +26,29 @@ public class JavaApplication1 {
         Scanner reader = new Scanner(System.in);
         System.out.println("What is your name");
         String name=reader.nextLine();
-        System.out.println("Your name was "+ name);
+        System.out.println("Your name is "+ name);
         
+        //Exercise Three
+        int length, height;
+        System.out.println("Enter Length: ");
+        name=reader.nextLine();
+        length=Integer.parseInt(name);
+        System.out.println("Enter Height: ");
+        name=reader.nextLine();
+        height=Integer.parseInt(name);
+        System.out.println("Perimeter="+(length+length+height+height));
+        System.out.println("Area="+(length*height));
+        
+        
+        //Exercise 4
+        float num1, num2;
+        System.out.println("Enter number 1:");
+        name=reader.nextLine();
+        num1=Float.parseFloat(name);
+        System.out.println("Enter number 2:");
+        name=reader.nextLine();
+        num2=Float.parseFloat(name);
+        System.out.println("Average of numbers is: "+((num1+num2)/2));
     
    /** public static void main(String[] args) {
         String name;
