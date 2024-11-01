0641f521415817c1f4d9459e4607c4343bc65c18
dmellor <dmellor@172.18.38.117>
2024-11-01 10:43:27 +0000

    help

---
+++ JavaApplication1/src/MusicPlayer/Song.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MusicPlayer;
+
+/**
+ *
+ * @author dmellor
+ * @version 1.0
+ * Purpose: The purpose of the class is to manage a song
+ */
+public class Song {
+    private String artistName;
+    private String songTitle;
+    private int playBack;
+    
+    /**
+     * Constructor: this constructor is used to build a blank song
+     */
+    public Song(){}
+    
+     /**
+     * Constructor: this constructor is used to a song with 
+     * the data which is required; artistName, songTitle, playBack
+     */
+    public Song(String artistName, String songTitle, int playBack){
+        this.artistName = artistName;
+        this.songTitle = songTitle;
+        this.playBack = playBack;
+        
+  
+                }
+    
+    /** 
+     * Method: this method will get the artists name 
+     */
+    
+    public String getArtistName(){
+        return this.artistName; 
+    
+    
+    }
+    
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+}
